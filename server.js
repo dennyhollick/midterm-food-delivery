b9cf69857ee94a17ed2d3537cda5ffe4e7b49c29
John <john.wong.h@gmail.com>
2017-07-20 21:10:00 +0000

    Added a route to load menu items on index page

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const knexLogger  = require('knex-logger');
 
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
+const menuRoutes = require("./routes/menu_items_route")
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -36,7 +37,7 @@ app.use("/styles", sass({
 app.use(express.static("public"));
 
 // Mount all resource routes
-app.use("/api/users", usersRoutes(knex));
+app.use("/api", menuRoutes(knex));
 
 // Home page
 app.get("/", (req, res) => {
