8e5d500b9ae229c906e5a4251c2376f7a7126bf9
Bryce <bryce.sayers-kwan@live.ca>
2017-07-21 21:53:10 +0000

    Added script file to append items to cart

--- public/scripts/create_cart.js
+++ public/scripts/create_cart.js
@@ -1,3 +1,25 @@
 $(() => {
-    
-})
+
+  function createItemElements(items) {
+    const cartItems = JSON.parse(window.localStorage.getItem('cart'));
+    var html = '';
+    for (var id in cartItems) {
+      var itemElement = `
+            <tr>
+              <td>${items[id - 1].name}</td>
+              <td class="text-right">${cartItems[id].amount}</td>
+              <td class="text-right">$${items[id - 1].price}</td>
+            </tr>
+        `;
+      html += itemElement;
+    }
+    return html;
+  }
+
+  $.ajax({
+    method: 'GET',
+    url: '/api/menu_items',
+  }).done((items) => {
+    $('.cart-items > tbody').append(createItemElements(items));
+  });
+});
