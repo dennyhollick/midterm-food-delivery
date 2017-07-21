09a3c634592ca53487e5fe87d637bc092694b6d4
Bryce <bryce.sayers-kwan@live.ca>
2017-07-21 18:24:32 +0000

    Changed script names and added a script file to handle cart items

--- public/scripts/set_cart.js
+++ public/scripts/set_cart.js
@@ -1,36 +1,3 @@
 $(() => {
-  const currentCart = JSON.parse(window.localStorage.getItem('cart'));
-
-  const cart = currentCart || {};
-
-  const helpers = {
-
-    addItems: (id, amount) => {
-
-      cart[id] = {
-        amount: amount
-      }
-
-      helpers.setCart(cart);
-
-    },
-
-    deleteItem: (id) => {
-
-      delete cart[id]
-
-      helpers.setCart(cart);
-    },
-
-    setCart: function (cart) {
-      window.localStorage.setItem('cart', JSON.stringify(cart));
-    }
-  };
-
-  $('body').on('click', '.add-item', function () {
-    let id = $(this).data('id');
-    let count = $(this).parents().next().val();
-    helpers.addItems(id, count);
-    console.log(currentCart);
-  });
+    
 })
