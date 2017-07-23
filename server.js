e269c4f7abbebfa53c77c452fe0ff00e72884ad1
Denny Hollick <dennyhollick@gmail.com>
2017-07-22 19:44:43 -0700

    Linted server.js to AirBnB styleguide

--- server.js
+++ server.js
@@ -1,60 +1,60 @@
-"use strict";
-
 require('dotenv').config();
 
-const PORT        = process.env.PORT || 8080;
-const ENV         = process.env.ENV || "development";
-const express     = require("express");
-const bodyParser  = require("body-parser");
-const sass        = require("node-sass-middleware");
-const app         = express();
+const PORT = process.env.PORT || 8080;
+const ENV = process.env.ENV || 'development';
+const express = require('express');
+const bodyParser = require('body-parser');
+const sass = require('node-sass-middleware');
 
-const knexConfig  = require("./knexfile");
-const knex        = require("knex")(knexConfig[ENV]);
-const morgan      = require('morgan');
-const knexLogger  = require('knex-logger');
+const app = express();
+
+const knexConfig = require('./knexfile');
+const knex = require('knex')(knexConfig[ENV]);
+const morgan = require('morgan');
+const knexLogger = require('knex-logger');
 
 // Seperated Routes for each Resource
-const menuRoutes = require("./routes/menu_items_route");
-const twilio = require("./routes/twilio");
+const menuRoutes = require('./routes/menu_items_route');
+const twilio = require('./routes/twilio');
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
-//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+// The :status token will be colored red for server error codes, yellow for client error codes,
+// cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
 
 // Log knex SQL queries to STDOUT as well
 app.use(knexLogger(knex));
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/styles", sass({
-  src: __dirname + "/styles",
-  dest: __dirname + "/public/styles",
+app.use('/styles', sass({
+  src: `${__dirname}/styles`,
+  dest: `${__dirname}/public/styles`,
   debug: true,
-  outputStyle: 'expanded'
+  outputStyle: 'expanded',
 }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // Mount all resource routes
-app.use("/api", menuRoutes(knex));
+app.use('/api', menuRoutes(knex));
 
 // Home page
-app.get("/", (req, res) => {
-  res.render("index");
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
 // Cart
 
-app.get("/cart", (req, res) => {
-  res.render("cart");
+app.get('/cart', (req, res) => {
+  res.render('cart');
 });
 
 app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
+  console.log(`Example app listening on port ${PORT}`);
 });
 
 // Twilio
 
-app.use("/twilio", twilio);
+app.use('/twilio', twilio);
 
