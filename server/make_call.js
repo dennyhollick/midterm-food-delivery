1967bd67978a3def09f017b6a910b33355b4c99f
Denny Hollick <dennyhollick@gmail.com>
2017-07-22 20:22:56 -0700

    Configured to get NGROK url from env instead of hardcoded one.

--- server/make_call.js
+++ server/make_call.js
@@ -1,6 +1,8 @@
 // This function initiates the call to a restaurant with an orderID as the input.
 // Use .env file to store Twilio account credentials and number to call.
 // Number format should be string (eg. '+17789981234')
+// While in development environment, need to download and configure NGROK in order to make
+// the localhost sever availible to the twilio API. Configure URL in .env
 
 require('dotenv').config({ path: '../.env' });
 
@@ -8,13 +10,14 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioPhone = process.env.TWILIO_PHONE_NUMBER;
 const recipient = process.env.NUMBER_TO_CALL;
+const developmentUrl = process.env.NGROK_DEVELOPMENT_URL;
 
 const client = require('twilio')(accountSid, authToken);
 
 function startCall(orderId) {
   client.calls.create({
 
-    url: `http://b5f5fbb1.ngrok.io/twilio?order_id=${orderId}`,
+    url: `${developmentUrl}/twilio?order_id=${orderId}`,
     to: recipient,
     from: twilioPhone,
   }, (err, call) => {
@@ -22,4 +25,4 @@ function startCall(orderId) {
   });
 }
 
-// startCall(2);
+startCall(2);
