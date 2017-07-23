88b0fd6355838330e4630d4a260276b84545b984
Denny Hollick <dennyhollick@gmail.com>
2017-07-22 19:39:42 -0700

    Removed phone number to call and replaced with process.env

--- server/make_call.js
+++ server/make_call.js
@@ -1,11 +1,13 @@
-
 // This function initiates the call to a restaurant with an orderID as the input.
+// Use .env file to store Twilio account credentials and number to call.
+// Number format should be string (eg. '+17789981234')
 
 require('dotenv').config({ path: '../.env' });
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioPhone = process.env.TWILIO_PHONE_NUMBER;
+const recipient = process.env.NUMBER_TO_CALL;
 
 const client = require('twilio')(accountSid, authToken);
 
@@ -13,7 +15,7 @@ function startCall(orderId) {
   client.calls.create({
 
     url: `http://b5f5fbb1.ngrok.io/twilio?order_id=${orderId}`,
-    to: '+17783864605',
+    to: recipient,
     from: twilioPhone,
   }, (err, call) => {
     process.stdout.write(call.sid);
