5ee7b6b0bcb251473005d90bf0abf7c07ee8151e
Denny Hollick <dennyhollick@gmail.com>
2017-07-23 16:32:52 -0700

    Updated to return all orders.

--- routes/orders.js
+++ routes/orders.js
@@ -5,16 +5,14 @@ const router  = express.Router();
 
 module.exports = (knex) => {
 
-  router.get("/orders/:id", (req, res) => {
+  router.get("/orders", (req, res) => {
     const orderId = req.params.id;
     knex
       .select("*")
       .from("orders")
-      .where('id', orderId)
       .then((results) => {
         res.json(results);
     });
   });
-
   return router;
 }
