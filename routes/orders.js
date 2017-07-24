de06dc1893a92e4cf692c8a5c8d94ee1aefcd6dd
Denny Hollick <dennyhollick@gmail.com>
2017-07-24 12:41:29 -0700

    Added comment and linted.

--- routes/orders.js
+++ routes/orders.js
@@ -1,18 +1,15 @@
-"use strict";
-
 const express = require('express');
-const router  = express.Router();
+
+const router = express.Router();
 
 module.exports = (knex) => {
-
-  router.get("/orders", (req, res) => {
-    const orderId = req.params.id;
+  router.get('/orders', (req, res) => {
     knex
-      .select("*")
-      .from("orders")
+      .select('*')
+      .from('orders')
       .then((results) => {
         res.json(results);
-    });
+      });
   });
   return router;
-}
+};
