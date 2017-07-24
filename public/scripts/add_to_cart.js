489cbcff813dc4fb5cfdaac1277da54cc783ccc9
Denny Hollick <dennyhollick@gmail.com>
2017-07-24 12:44:03 -0700

    Linted.

--- public/scripts/add_to_cart.js
+++ public/scripts/add_to_cart.js
@@ -6,33 +6,29 @@ $(() => {
   const helpers = {
 
     addItems: (name, amount) => {
-
       cart[name] = {
         amount,
-      }
+      };
 
       helpers.addToCart(cart);
-
     },
 
     deleteItem: (id) => {
-
-      delete cart[id]
+      delete cart[id];
 
       helpers.addToCart(cart);
     },
 
-    addToCart: function (cart) {
+    addToCart(cart) {
       window.localStorage.setItem('cart', JSON.stringify(cart));
-    }
+    },
   };
 
   $('body').on('click', '.add-item', function () {
-    swal("Item Added!", "You can add more or go to your cart!", "success");
-    let name = $(this).data('name');
-    let count = $(this).parent().next().val();
+    swal('Item Added!', 'You can add more or go to your cart!', 'success');
+    const name = $(this).data('name');
+    const count = $(this).parent().next().val();
     helpers.addItems(name, count);
-    console.log(currentCart);
   });
-})
+});
 
