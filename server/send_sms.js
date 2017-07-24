0dc767cfe51af1c15470d4a913a7a5f460323d1e
Denny Hollick <dennyhollick@gmail.com>
2017-07-24 12:31:32 -0700

    Removed redundant comments

--- server/send_sms.js
+++ server/send_sms.js
@@ -8,21 +8,21 @@ const recipient = process.env.NUMBER_TO_CALL;
 const client = require('twilio')(accountSid, authToken);
 
 module.exports = function sendClientNotifications(phone, orderTime) {
-        // Create options to send the message
+// Create options to send the message
   const options = {
     to: phone,
     from: twilioPhone,
-            /* eslint-disable max-len */
+/* eslint-disable max-len */
     body: `Your order has been confirmed! You can pick it up in ${orderTime} minutes`,
-            /* eslint-enable max-len */
+/* eslint-enable max-len */
   };
 
-        // Send the message!
-  client.messages.create(options, (err, response) => {
+// Send the message!
+  client.messages.create(options, (err) => {
     if (err) {
       console.error(err);
     } else {
-        // Log the last few digits of a phone number
+// Log the last few digits of a phone number
       let masked = recipient.substr(0,
                     recipient.length - 5);
       masked += '*****';
@@ -30,10 +30,4 @@ module.exports = function sendClientNotifications(phone, orderTime) {
     }
   });
 };
-
-    // Don't wait on success/failure, just indicate all messages have been
-    // queued for delivery
-  // if (callback) {
-  //   callback.call();
-  // }
 
